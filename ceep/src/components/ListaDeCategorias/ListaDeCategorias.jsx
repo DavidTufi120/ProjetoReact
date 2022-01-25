8f4aaa79a2c82dae8ebacd504c031d02192d2ca3
DavidTufi120 <davizinhol_@hotmail.com>
2022-01-25 14:09:28 -0300

    Implementar campo e label categoria

---
+++ ceep/src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import "./estilo.css"
+class ListaDeCategorias extends Component {
+    state = {  } 
+
+    _handleEventoInput(e){
+        console.log(e.key)
+            if(e.key == "Enter"){
+                console.log("adicionar categoria")
+
+            }
+        }
+
+    
+    render() { 
+        return (
+        <section className="lista-categorias">
+        <ul className='lista-categorias_lista'>
+            <li className="lista-categorias_item">Categorias</li>
+            <li className="lista-categorias_item">Categorias</li>
+            <li className="lista-categorias_item">Categorias</li>
+            <li className="lista-categorias_item">Categorias</li>
+        </ul>
+        
+        <input type="text" 
+        className='lista-categorias_input' 
+        placeholder='Adicionar Categoria'
+        onKeyUp={this._handleEventoInput.bind(this)}
+         />
+
+        </section>
+        )
+        ;
+    }
+}
+ 
+export default ListaDeCategorias;
